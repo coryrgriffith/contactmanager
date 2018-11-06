be0de0dd15df4a570980ab3ff7940fd72f34f6ea
coryrgriffith <corygriffith9@gmail.com>
2018-11-06 12:03:05 -0600

    add javascript example of React element to compare to jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    // javascript version
+
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "The App Component")
+    // );
+
+    // jsx version
     return (
       <div className="App">
         <h1>The App Component</h1>
