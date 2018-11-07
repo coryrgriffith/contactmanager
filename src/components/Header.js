def69952fb4a4cc2de7865ac37b80bdc2821715b
coryrgriffith <corygriffith9@gmail.com>
2018-11-07 11:35:29 -0600

    destructure in header component and refactor code for branding

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,10 @@ import React from "react";
 
 // arrow function
 const Header = props => {
+  const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 };
