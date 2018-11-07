3a835c4d0ebffd256b723c2ffa9f6c9729cf9892
coryrgriffith <corygriffith9@gmail.com>
2018-11-07 11:29:47 -0600

    add contact information to props and pass to contact component

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
         {/* {showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
         {math} */}
       </div>
