4246130434d0f4d90a8bce036dc53174f6f7c937
coryrgriffith <corygriffith9@gmail.com>
2018-11-08 11:26:24 -0600

    refactor Contacts component to import all of the Contact component object

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -31,12 +31,7 @@ class Contacts extends Component {
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
