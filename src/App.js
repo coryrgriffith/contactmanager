bc6d3ca590af9fe4f972cbd2fedab1c31e6ab728
coryrgriffith <corygriffith9@gmail.com>
2018-11-08 10:47:44 -0600

    add cards to Contact component, wrap main app components in container to add spacing to everything but the header

--- src/App.js
+++ src/App.js
@@ -36,14 +36,20 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
-        {/* {showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
-        {math} */}
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+          {/* {showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
+          {math} */}
+        </div>
       </div>
     );
   }
