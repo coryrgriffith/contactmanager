4153e57f0176f35a337150d496c720a99bfbb716
coryrgriffith <corygriffith9@gmail.com>
2018-11-08 11:28:07 -0600

    refactor Contact component to destructure contact object

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,13 +11,13 @@ class Contact extends Component {
   // };
 
   render() {
-    const { contact } = this.props;
+    const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4>
+        <h4>{name}</h4>
         <ul className="list-group">
-          <li className="list-group-item">Email: {contact.email}</li>
-          <li className="list-group-item">Phone: {contact.phone}</li>
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
