fe4f1084808627fce6f29ee7a47c045d678c011c
coryrgriffith <corygriffith9@gmail.com>
2018-11-08 12:08:20 -0600

    refactor Contacts component to remove extra div by using React Fragment and remove the testing function from onShowClick function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,20 +11,14 @@ class Contact extends Component {
   // };
   state = {};
 
-  onShowClick = (name, e) => {
-    console.log(name);
-  };
+  onShowClick = e => {};
 
   render() {
     const { name, email, phone } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name}{" "}
-          <i
-            onClick={this.onShowClick.bind(this, name)}
-            className="fas fa-sort-down"
-          />
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
