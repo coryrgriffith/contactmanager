2a925123cb627e273071bc4467bbed1ae7fabefd
coryrgriffith <corygriffith9@gmail.com>
2018-11-14 11:51:07 -0600

    in Contact component use state to toggle the email and phone number displayed for each individual contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,6 +15,8 @@ class Contact extends Component {
 
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
         <h4>
@@ -26,10 +28,12 @@ class Contact extends Component {
             className="fas fa-sort-down"
           />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
