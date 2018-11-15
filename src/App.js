11b2d87a53210673d5bf96ad56363e145c555ae4
coryrgriffith <corygriffith9@gmail.com>
2018-11-15 15:33:34 -0600

    add AddContact component which contains a form to add a new contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
 
 import { Provider } from "./context";
 
@@ -40,6 +41,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
             {/* {showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
           {math} */}
