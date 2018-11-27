443957d163da73a0dd2721c0645c25bdfcd7679b
coryrgriffith <corygriffith9@gmail.com>
2018-11-27 11:26:56 -0600

    add lifecycle methods to Test component

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,6 +1,48 @@
 import React, { Component } from "react";
 
 class Test extends Component {
+  state = {
+    test: "test"
+  };
+
+  // runs after component is mounted
+  componentDidMount() {
+    console.log("componentDidMount...");
+  }
+
+  // runs before component renders
+  // Legacy Lifecycle
+  componentWillMount() {
+    console.log("componentWillMount...");
+  }
+
+  // runs when component is updated
+  componentDidUpdate() {
+    console.log("componentDidUpdate...");
+  }
+
+  // Legacy Lifecycle
+  componentWillUpdate() {
+    console.log("componentWillUpdate...");
+  }
+
+  // runs when component receives new props
+  // Legacy Lifecycle
+  componentWillReceiveProps(nextProps, nextState) {
+    console.log("componentWillReceiveProps...");
+  }
+
+  // must return state or null
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return {
+      test: "something"
+    };
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate...");
+  }
+
   render() {
     return (
       <div>
