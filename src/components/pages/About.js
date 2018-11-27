0ec96b242519ed5d95b081a74294694b11162938
coryrgriffith <corygriffith9@gmail.com>
2018-11-27 10:50:25 -0600

    remove params from About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default props => {
+export default function About() {
   return (
     <div>
-      <h1 className="display-4">{props.match.params.id}</h1>
+      <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple app to manage contacts</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
   );
-};
+}
