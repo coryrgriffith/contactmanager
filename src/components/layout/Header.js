a85335e36106b746286608f3a804eed515205d5f
coryrgriffith <corygriffith9@gmail.com>
2018-11-27 10:48:29 -0600

    add params on about page to display

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -25,7 +25,17 @@ const Header = props => {
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link to="/" className="nav-link">
-              Home
+              <i className="fas fa-home" />Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact/add" className="nav-link">
+              <i className="fas fa-plus" />Add Contact
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              <i className="fas fa-question" />About
             </Link>
           </li>
         </ul>
