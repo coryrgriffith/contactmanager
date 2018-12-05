fb55b5df1b191ac4879decc94ad45172461deda9
coryrgriffith <corygriffith9@gmail.com>
2018-12-05 17:07:37 -0600

    update the edit contact to send an axios put response that updates the data in the api

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -46,6 +46,21 @@ class EditContact extends Component {
       return;
     }
 
+    const updateContact = {
+      name,
+      email,
+      phone
+    };
+
+    const { id } = this.props.match.params;
+
+    const response = await axios.put(
+      `http://jsonplaceholder.typicode.com/users/${id}`,
+      updateContact
+    );
+
+    dispatch({ type: "UPDATE_CONTACT", payload: response.data });
+
     this.setState({
       name: "",
       email: "",
