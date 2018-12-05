c5559a8f049c71ee6cd66b1e5e347851783dfd8c
coryrgriffith <corygriffith9@gmail.com>
2018-12-05 16:06:55 -0600

    refactor sxios request to use async

--- src/context.js
+++ src/context.js
@@ -30,10 +30,12 @@ export class Provider extends Component {
     }
   };
 
-  componentDidMount() {
-    axios
-      .get("http://jsonplaceholder.typicode.com/users")
-      .then(response => this.setState({ contacts: response.data }));
+  async componentDidMount() {
+    const response = await axios.get(
+      "http://jsonplaceholder.typicode.com/users"
+    );
+
+    this.setState({ contacts: response.data });
   }
 
   render() {
